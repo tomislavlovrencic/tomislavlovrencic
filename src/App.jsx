2f4b7ded9a623c5eb23c0600a54de7e8e12a0fe7
tomislavlovrencic <tomislav.lovrencic@happening.xyz>
2024-08-08 13:33:38 +0200

    minor restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import reactImg from './assets/react-core-concepts.png'
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  ); 
-}
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
     </div>
   );
 }
