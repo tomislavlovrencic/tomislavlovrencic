0854ddda2b6dae710f209e6148f6d18b9b14763f
tomislavlovrencic <tomislav.lovrencic@happening.xyz>
2024-08-12 14:44:19 +0200

    css for home page a bit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,12 +5,13 @@ function Home() {
   return (
     <div className="home-container">
       <div className="name-container">
-        <h1 className="home-firstname">Tomislav :)</h1>
+        <h1 className="home-hi">Hi, I'm</h1>
+        <h1 className="home-firstname">Tomislav</h1>
         <h1 className="home-lastname">Lovrenčić</h1>
       </div>
       <div className="description-container">
         <p className="home-description">
-          I'm a <span className="highlight">Software Engineer</span> experienced in <span className="highlight">Java</span>, <span className="highlight">Golang</span>, <span className="highlight">JavaScript</span>, <span className="highlight">React</span>, and much more. I love going the extra mile and have a passion for creating modern and efficient web applications.
+          <span className="highlight">Software Developer</span> experienced in <span className="highlight">Java</span>, <span className="highlight">Golang</span>, <span className="highlight">JavaScript</span>, <span className="highlight">React</span>, and much more. I love going the extra mile and have a passion for solving complex real world's problems.
         </p>
         <div className="button-container">
           <a href="#!" className="custom-button">Download CV</a>
