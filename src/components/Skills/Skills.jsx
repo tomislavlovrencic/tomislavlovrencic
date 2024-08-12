8dc5f1a12465346fce7648bfff8da74940137344
tomislavlovrencic <tomislav.lovrencic@happening.xyz>
2024-08-12 14:20:08 +0200

    Skills updated

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import './Skills.css';
 import { LinearProgress, Box, Typography } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faJava, 
+  faReact, 
+  faPython, 
+  faDocker,
+  faGit,
+  faGolang,
+  faAws,
+} from '@fortawesome/free-brands-svg-icons';
+
+import { faDatabase, faCircleNodes, faCloudBolt } from '@fortawesome/free-solid-svg-icons';
+
 
 function Skills() {
   const skills = [
     {
       category: 'Programming Languages',
       items: [
-        { name: 'Java', level: 95 },
-        { name: 'Golang', level: 85 },
-        { name: 'React', level: 60 },
-        { name: 'Python', level: 80 },
-        { name: 'SQL', level: 90 },
-        { name: 'C++', level: 84 },
+        { name: 'Java', level: 95, icon: faJava },
+        { name: 'Golang', level: 85, icon: faGolang }, 
+        { name: 'React', level: 60, icon: faReact },
+        { name: 'Python', level: 80, icon: faPython },
+        { name: 'SQL', level: 90, icon: faDatabase }, 
       ],
     },
     {
       category: 'Industry Software Skills',
       items: [
-        { name: 'Kafka', level: 90 },
-        { name: 'AWS', level: 80 },
-        { name: 'Version Control (Git)', level: 95 },
-        { name: 'Docker', level: 85 },
-        { name: 'Kubernetes', level: 80 },
-        { name: 'Prometheus', level: 85 },
+        { name: 'Kafka', level: 90, icon: faDatabase },
+        { name: 'AWS', level: 80, icon: faAws },
+        { name: 'Version Control (Git)', level: 95, icon: faGit },
+        { name: 'Docker', level: 85, icon: faDocker },
+        { name: 'Kubernetes', level: 80, icon: faCircleNodes },
+        { name: 'Prometheus', level: 85, icon: faCloudBolt }, 
       ],
     },
   ];
@@ -38,9 +50,12 @@ function Skills() {
           {skillGroup.items.map((skill, idx) => (
             <Box key={idx} className="skill-item">
               <Box className="skill-info">
-                <Typography variant="body1" className="skill-name">
-                  {skill.name}
-                </Typography>
+                <Box className="skill-name-icon">
+                  <FontAwesomeIcon icon={skill.icon} className="skill-icon" />
+                  <Typography variant="body1" className="skill-name">
+                    {skill.name}
+                  </Typography>
+                </Box>
                 <Typography variant="body1" className="skill-level">
                   {skill.level}%
                 </Typography>
