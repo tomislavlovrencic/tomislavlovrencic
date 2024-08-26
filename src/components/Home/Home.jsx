982c37163d88967d996e3020d9b085ab0d89ef4c
tomislavlovrencic <tomislav.lovrencic@happening.xyz>
2024-08-26 11:42:53 +0200

    added few tweaks

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,17 +5,16 @@ function Home() {
   return (
     <div className="home-container">
       <div className="name-container">
-        <h1 className="home-hi">Hi, I'm</h1>
         <h1 className="home-firstname">Tomislav</h1>
         <h1 className="home-lastname">Lovrenčić</h1>
-      </div>
-      <div className="description-container">
-        <p className="home-description">
-          <span className="highlight">Software Developer</span> experienced in <span className="highlight">Java</span>, <span className="highlight">Golang</span>, <span className="highlight">JavaScript</span>, <span className="highlight">React</span>, and much more. I love going the extra mile and have a passion for solving complex real world's problems.
-        </p>
-        <div className="button-container">
-          <a href="#!" className="custom-button">Download CV</a>
-          <a href="#!" className="custom-button">GitHub</a>
+        <div className="description-container">
+          <p className="home-description">
+            <span className="highlight">Software Developer</span> with a strong foundation in <span className="highlight">Java</span>, <span className="highlight">Golang</span>, <span className="highlight">JavaScript</span>, and <span className="highlight">React</span>. My expertise lies in designing and implementing scalable microservices, optimizing system architectures, and enhancing software performance. I have a proven track record of identifying and resolving critical system issues, and I’m always eager to go the extra mile.
+          </p>
+          <div className="button-container">
+            <a href="#!" className="custom-button">Download CV</a>
+            <a href="#!" className="custom-button">GitHub</a>
+          </div>
         </div>
       </div>
     </div>
