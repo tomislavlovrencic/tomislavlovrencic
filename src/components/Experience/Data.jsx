b49114b06d98dfc5969a11eb4b8077a1f832276b
tomislavlovrencic <tomislav.lovrencic@happening.xyz>
2024-08-28 14:41:28 +0200

    text enrichment

--- src/components/Experience/Data.jsx
+++ src/components/Experience/Data.jsx
@@ -1,49 +1,49 @@
 const experienceData = [
-    {
-      date: 'July 2022 – Present',
-      title: 'Superbet',
-      subtitle: 'Software Development Engineer',
-      responsibilities: [
-        'Currently designing and implementing microservices across various domains as a backend developer at Superbet.',
-        'Participated in the design and implementation of a new betting architecture, incorporating generic SDK plugins for efficient parallel bet handling, simplifying service integration.',
-        'Directly identified and resolved two critical exploits in our core systems, thus saving the company significant financial resources.',
-        'Managed the deployment of 20+ services for Superbet’s international expansion, adapting databases, message queues, and system architectures to meet diverse regional requirements.',
-      ],
-    },
-    {
-      date: 'July 2020 – July 2022',
-      title: 'Autodesk',
-      subtitle: 'Software Development Engineer',
-      responsibilities: [
-        'Contributed to the development of the world’s leading PLM software, enhancing its scalability and distribution capabilities.',
-        'Implemented an efficient caching layer using Redis, significantly enhancing the PLM software’s performance. This strategic addition reduced data retrieval times by 40%.',
-        'Achieved a 75% reduction in the loading time of the Autodesk home page by implementing parallel processing and optimizing tree node algorithms.',
-      ],
-    },
-    {
-      date: 'September 2021 – July 2023',
-      title: 'Faculty of Electrical Engineering and Computing',
-      subtitle: 'Master’s Degree in Computer Science',
-      responsibilities: [
-        'Explored the use of Generative Adversarial Networks (GANs) for colorizing black and white images. Analyzed the complex architecture of GANs and conducted iterative experimentation with various loss functions and optimization techniques to refine the model.',
-      ],
-    },
-    {
-      date: 'September 2018 – July 2021',
-      title: 'Faculty of Electrical Engineering and Computing',
-      subtitle: 'Bachelor’s Degree in Computer Science',
-      responsibilities: [
-        'Developed and implemented a face recognition algorithm based on Adam Geitgey\'s approach. Achieved 100% recognition accuracy in controlled environments, demonstrating robustness against variations in light, contrast, and face poses.',
-      ],
-    },
-    {
-      date: 'July 2018 – December 2018',
-      title: 'Infinum Academy',
-      subtitle: 'Android Developer',
-      responsibilities: [
-        'Completed a comprehensive program in Android development, focusing on Kotlin and Java, and acquired hands-on experience in mobile application development.',
-      ],
-    },
-  ];
+  {
+    date: 'July 2022 – Present',
+    title: 'Superbet',
+    subtitle: 'Software Development Engineer',
+    responsibilities: [
+      'Currently working as a backend developer at Superbet, where I’m involved in designing and building microservices across different domains. My focus is on creating robust, scalable systems that can handle the demands of a rapidly growing betting platform.',
+      'One of my biggest contributions has been helping design and implement a new betting platform architecture. I worked on integrating generic SDK plugins to streamline parallel bet processing, making it easier to integrate services and improving efficiency.',
+      'I’ve also played a role in securing our systems, personally identifying and resolving two critical exploits that had a financial cost to the company.',
+      'In addition to development, I’ve managed the deployment of over 20 services as part of Superbet’s international expansion. This involved adapting everything from databases to message queues to ensure our architecture could meet the unique requirements of each new region.',
+    ],
+  },
+  {
+    date: 'July 2020 – July 2022',
+    title: 'Autodesk',
+    subtitle: 'Software Development Engineer',
+    responsibilities: [
+      'At Autodesk, I was part of the team behind the world’s leading Product Lifecycle Management (PLM) software. My work primarily focused on improving the software’s scalability and distribution, which was critical given the global user base.',
+      'Led an initiative to optimize data storage per user by implementing advanced compression techniques using existing libraries. This effort resulted in a 30% reduction in data transfer times, significantly enhancing the end-user experience by making the product more responsive and efficient.',
+      'I also tackled the challenge of optimizing the user dashboard load time, where all project data, including nested directories, had to be loaded and calculated. By restructuring the data storage to aggregate and store key information at the root nodes of each project, I reduced the complexity of data retrieval. This strategic maneuver cut the dashboard loading time by 75%, significantly improving the user experience despite a slight increase in write operation overheads.',
+    ],
+  },
+  {
+    date: 'September 2021 – July 2023',
+    title: 'Faculty of Electrical Engineering and Computing',
+    subtitle: 'Master’s Degree in Computer Science',
+    responsibilities: [
+      'Explored the use of Generative Adversarial Networks (GANs) for colorizing black and white images. Analyzed the complex architecture of GANs and conducted iterative experimentation with various loss functions and optimization techniques to refine the model.',
+    ],
+  },
+  {
+    date: 'September 2018 – July 2021',
+    title: 'Faculty of Electrical Engineering and Computing',
+    subtitle: 'Bachelor’s Degree in Computer Science',
+    responsibilities: [
+      'Developed and implemented a face recognition algorithm based on Adam Geitgey\'s approach. Achieved 100% recognition accuracy in controlled environments, demonstrating robustness against variations in light, contrast, and face poses.',
+    ],
+  },
+  {
+    date: 'July 2018 – December 2018',
+    title: 'Infinum Academy',
+    subtitle: 'Android Developer',
+    responsibilities: [
+      'Completed a comprehensive program in Android development, focusing on Kotlin and Java, and acquired hands-on experience in mobile application development.',
+    ],
+  },
+];
 
-  export default experienceData;
+export default experienceData;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
         <h1 className="home-lastname">Lovrenčić</h1>
         <div className="description-container">
           <p className="home-description">
-            <span className="highlight">Software Developer</span> with a strong foundation in <span className="highlight">Java</span>, <span className="highlight">Golang</span>, <span className="highlight">JavaScript</span>, and <span className="highlight">React</span>. My expertise lies in designing and implementing scalable microservices, optimizing system architectures, and enhancing software performance. I have a proven track record of identifying and resolving critical system issues, and I’m always eager to go the extra mile.
+            <span className="highlight">Software Developer</span> with a strong foundation in <span className="highlight">Java</span> and <span className="highlight">Golang</span>, and a growing expertise in <span className="highlight">JavaScript</span> and <span className="highlight">React</span>. Through dedicated self-learning and hands-on experience, I've developed this webpage as a side project to deepen my skills in React. My strengths lie in designing and implementing scalable microservices, optimizing system architectures, and enhancing software performance. I’m committed to continuous learning and always eager to take on new challenges.
           </p>
           <div className="button-container">
             <a href="#!" className="custom-button">Download CV</a>
