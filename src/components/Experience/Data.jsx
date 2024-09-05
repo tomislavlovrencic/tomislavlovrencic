0bafb67e1d5dbba9fb9d9a4c51060df5d08dbaf0
tomislavlovrencic <tomislav.lovrencic@happening.xyz>
2024-09-05 11:45:42 +0200

    add to github pages

--- src/components/Experience/Data.jsx
+++ src/components/Experience/Data.jsx
@@ -5,7 +5,7 @@ const experienceData = [
     subtitle: 'Software Development Engineer',
     responsibilities: [
       'Currently working as a backend developer at Superbet, where I’m involved in designing and building microservices across different domains. My focus is on creating robust, scalable systems that can handle the demands of a rapidly growing betting platform.',
-      'One of my biggest contributions has been helping design and implement a new betting platform architecture. I worked on integrating generic SDK plugins to streamline parallel bet processing, making it easier to integrate services and improving efficiency.',
+      'One of my biggest contributions has been helping design and implement a new betting platform architecture. I worked on integrating generic SDK plugins to streamline parallel bet processing, making it easier to integrate services and improve efficiency.',
       'I’ve also played a role in securing our systems, personally identifying and resolving two critical exploits that had a financial cost to the company.',
       'In addition to development, I’ve managed the deployment of over 20 services as part of Superbet’s international expansion. This involved adapting everything from databases to message queues to ensure our architecture could meet the unique requirements of each new region.',
     ],
@@ -17,7 +17,7 @@ const experienceData = [
     responsibilities: [
       'At Autodesk, I was part of the team behind the world’s leading Product Lifecycle Management (PLM) software. My work primarily focused on improving the software’s scalability and distribution, which was critical given the global user base.',
       'Led an initiative to optimize data storage per user by implementing advanced compression techniques using existing libraries. This effort resulted in a 30% reduction in data transfer times, significantly enhancing the end-user experience by making the product more responsive and efficient.',
-      'I also tackled the challenge of optimizing the user dashboard load time, where all project data, including nested directories, had to be loaded and calculated. By restructuring the data storage to aggregate and store key information at the root nodes of each project, I reduced the complexity of data retrieval. This strategic maneuver cut the dashboard loading time by 75%, significantly improving the user experience despite a slight increase in write operation overheads.',
+      'I also tackled the challenge of optimizing the user dashboard load time, where all project data, including nested directories, had to be loaded and calculated. By restructuring the data storage to aggregate and store key information at the root nodes of each project, I reduced the complexity of data retrieval. This optimization cut the dashboard loading time by 75%, significantly improving the user experience despite a slight increase in write operation overheads.',
     ],
   },
   {
